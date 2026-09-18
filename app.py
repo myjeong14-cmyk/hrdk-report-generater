e826ec2ca8083ab404823ba157d482ed6ffbfba8
myjeong14-cmyk <myjeong14@gmail.com>
2026-09-18 10:52:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -867,7 +867,7 @@ col5, col6 = st.columns(2)
 with col5:
     daily_fee = st.number_input("일비", 0, step=1000)
 with col6:
-    meal_fee = st.number_input("식비", 25000, step=1000)
+    meal_fee = st.number_input("식비", 0, step=1000)
 
 matched_img_file = find_matched_map_image(dest_selection) if dest_selection != ADD_DEST_OPTION else None
 
